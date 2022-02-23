bc28432b77101bcc87ab3be26226c249ad872a91
santosraquel <raquel.dossantos1965@gmail.com>
2022-02-23 20:36:05 -0300

    register permissions register and papel record

---
+++ reservaonline/src/main/java/com/reservaonline/repositorios/PapelRepositorio.java
@@ -0,0 +1,9 @@
+package com.reservaonline.repositorios;
+
+import com.reservaonline.modelos.Papel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PapelRepositorio extends JpaRepository<Papel, Long> {
+
+}
---
+++ reservaonline/src/main/java/com/reservaonline/repositorios/PermissaoRepositorio.java
@@ -0,0 +1,9 @@
+package com.reservaonline.repositorios;
+
+import com.reservaonline.modelos.Permissao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissaoRepositorio extends JpaRepository<Permissao, Long> {
+
+}
