a1a5085c1ff7a64f16a8313eaa7c910cdd92d0da
costarafa <rafaelacs.rc@gmail.com>
2022-02-23 23:24:38 -0300

    modelo controle

---
+++ reservaonline/src/main/java/com/reservaonline/modelos/Quarto.java
@@ -0,0 +1,78 @@
+package com.reservaonline.modelos;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "quarto")
+public class Quarto implements Serializable {
+
+	public Quarto() {
+		super();
+	}
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String tipo;
+	private Double preco;
+	private String descricao;
+	private String status;
+	private String nomeImagem;
+	
+
+	public String getNomeImagem() {
+        return nomeImagem;
+    }
+
+    public void setNomeImagem(String nomeImagem) {
+        this.nomeImagem = nomeImagem;
+    }
+
+    public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
---
+++ reservaonline/src/main/java/com/reservaonline/repositorios/QuartoRepositorio.java
@@ -0,0 +1,9 @@
+package com.reservaonline.repositorios;
+
+import com.reservaonline.modelos.Quarto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuartoRepositorio extends JpaRepository<Quarto, Long> {
+
+}
